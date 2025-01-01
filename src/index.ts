ebbcc020c1a3195b7ba3cee2edf0b1407199ac9e
nicoacosta.eth <nicoacosta8@gmail.com>
2025-01-01 06:12:31 -0300

    feat(app): add prettyJSON

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
 
+app.use('*', prettyJSON())
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+	return c.text('Hello Hono!')
 })
 
 export default app
