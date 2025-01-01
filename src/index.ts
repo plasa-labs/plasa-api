d04f8464b1376cc519735922ee2680487c2f9d82
nicoacosta.eth <nicoacosta8@gmail.com>
2025-01-01 06:13:02 -0300

    feat(app): add logger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import { prettyJSON } from 'hono/pretty-json'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
 
 app.use('*', prettyJSON())
+app.use('*', logger())
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!')
